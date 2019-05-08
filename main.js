c16bed6b99fd9c45a099f3f83ced7b9eaa06866d
Alexander <blacksonj7@gmail.com>
2019-05-08 22:00:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,55 +12,14 @@
 const utils = require('@iobroker/adapter-core');
 const adapter = new utils.Adapter('countdown');
 
-let adapter;
 
 function startAdapter(options) {
     adapter.log.info('start adapter');
 
-    options = options || {};
-    Object.assign(options, {
-      name: 'hs100',
-      
-      stateChange: function (id, state) {
-          setDevState(id, state); 
-      },
-        
-      ready: function () {
-        main();
-      },
-      
-      unload: function (callback) {
-        if (timer) {
-          clearInterval(timer);
-          timer = 0;
-        }
-        isStopping = true;
-        callback && callback();
-      },
-         
-      objectChange: function (id, obj) {
-         adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-      }     
-    });
-    adapter = new utils.Adapter(options);
-    
-    return adapter;
 };
 
 function stop() {
     adapter.log.info('stop adapter');
-
-    if (stopTimer) clearTimeout(stopTimer);
-
-    // Stop only if schedule mode
-    if (adapter.common && adapter.common.mode == 'schedule') {
-        stopTimer = setTimeout(function () {
-            stopTimer = null;
-            if (timer) clearInterval(timer);
-            isStopping = true;
-            adapter.stop();
-        }, 30000);
-    }
 }
 
 // Load your modules here, e.g.:
