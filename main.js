22571fb7a889e056f3a75821d6523272223eee99
Alexander <blacksonj7@gmail.com>
2019-06-07 14:42:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ function loopsetup(){
 
 function getVariableTranslation(){
     var language = ''
-    adapter.getObject('system.config', (err, systemConfig) => {
+    adapter.getForeignObject('system.config', (err, systemConfig) => {
         language = systemConfig.common.language
 
         //var arrlineLong = [id1.common.name.replace,state.val];
